91fb4ec6c6212bb99c99dc29c26d427cb7067869
cat32tw <mike1993xd@gmail.com>
2022-03-22 23:16:30 +0800

    範圍上下鈕控制項練習

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace insect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(413, 330);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Location = new System.Drawing.Point(431, 12);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(246, 25);
+            this.domainUpDown1.TabIndex = 2;
+            this.domainUpDown1.Text = "domainUpDown1";
+            this.domainUpDown1.SelectedItemChanged += new System.EventHandler(this.domainUpDown1_SelectedItemChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(435, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 290);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 359);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace insect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            domainUpDown1.Items.Add("叩頭蟲");
+            domainUpDown1.Items.Add("吉丁蟲");
+            domainUpDown1.Items.Add("鍬形蟲");
+            domainUpDown1.Items.Add("獨角仙");
+            domainUpDown1.Items.Add("寬尾鳳蝶");
+            domainUpDown1.SelectedIndex = 0;
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+            string[] msg = new string[] {"胸腹間有彈器可以瞬間彈跳，以利翻身或避敵。",
+                                        "吉丁蟲被稱為「飛舞的寶石」。",
+                                        "頭上具有一對鹿角狀大顎，是牠們禦敵的武器。",
+                                        "因頭部有大角故稱為獨角仙，是台灣第二大甲蟲。",
+                                        "有「國蝶」之稱，被列為瀕臨絕種的一級保護物種。"};
+            pictureBox1.Image = Image.FromFile(@"D:\cat32tw\Desktop\insect\images\" + domainUpDown1.SelectedItem.ToString() + ".jpg");
+            label1.Text = msg[domainUpDown1.SelectedIndex];
+        }
+    }
+}
